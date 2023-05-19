a97b428bcecf68a122413399337b43df9bb83b77
GitHubBoKe <1311998370@qq.com>
2023-05-19 08:19:41 +0800

    初始化仓库

---
+++ get_ipv6_email.py
@@ -0,0 +1,81 @@
+import os
+import json
+import socket
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.text import MIMEText
+import time
+import smtplib
+
+
+def get_ip():
+
+    hostname = socket.gethostname()
+
+
+    addr_infos = socket.getaddrinfo(hostname, None, socket.AF_INET6)
+    ips = set([addr_info[-1][0] for addr_info in addr_infos])
+    print(ips)
+    # 获取一个ipv6地址列表
+    urlText = [ip for ip in ips if ip.startswith("24")] #获取24开头的ipv6地址
+    # 给ipv6列表添加http:[]
+    global_ips=['http://[' + item + ']' for item in urlText]
+    # global_ips = ips
+    whether_to_send, send_ip = get_temp_ip(global_ips)
+    send_ip = json.dumps(send_ip)
+    return whether_to_send, send_ip
+    
+    
+def get_temp_ip(current_ip):
+    # temp_ip_json_path = "/var/tmp/ip.json"
+    temp_ip_json_path = "ip.json"
+    if not os.path.exists(temp_ip_json_path):
+        print("没有存储个过ip,发送并存储它.".format(temp_ip_json_path))
+        with open(temp_ip_json_path, 'w') as jo:
+            json.dump(current_ip, jo)
+            return True, current_ip
+
+    else:
+        with open(temp_ip_json_path, 'r') as jo:
+            origin_ip = json.load(jo)
+        if origin_ip == current_ip:
+            # print("Current ip {} do not change, no need to send".format(current_ip))
+            print("当前ip地址没有改变,不需要发送邮件".format(current_ip))
+            return False, current_ip
+        else:
+            # print("The ip updated from {} to {}, update it.".format(origin_ip, current_ip))
+            print("当前ip地址发送改变,发送邮件通知.".format(origin_ip, current_ip))
+            os.remove(temp_ip_json_path)
+            with open(temp_ip_json_path, 'w') as jo:
+                json.dump(current_ip, jo)
+                return True, current_ip
+
+
+    
+def send_an_email(email_content): # email_content是一个字符串
+    mail_host = "smtp.163.com" # 这个去邮箱找
+    mail_user = '1xxxxx0@163.com' #发送的邮件地址
+    mail_auth_code = 'YxxxxxxBH' #授权码，不是邮箱的登陆密码
+    mail_sender = mail_user # 用mail_user 作为发送人
+    mail_receivers = ['1xxxxxx0@qq.com'] #收件人list
+    message = MIMEMultipart()
+    message['From'] = Header(mail_sender)  # 寄件人
+    message['Subject'] = Header("随身wifi的ipv6地址") #主题名字
+    message.attach(MIMEText(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())+'\n', 'plain', 'utf-8')) # 时间
+    message.attach(MIMEText('该随身wifi的ipv6地址如下:\n', 'plain', 'utf-8')) # 头信息
+    
+    # 具体的IPv6地址
+    message.attach(MIMEText(email_content, 'plain', 'utf-8'))
+    # print("message is {}".format(message.as_string())) # debug用
+    smtpObj = smtplib.SMTP(mail_host)
+    # smtpObj.set_debuglevel(1) # 同样是debug用的
+    smtpObj.login(mail_user, mail_auth_code) # 登陆
+    smtpObj.sendmail(mail_sender, mail_receivers, message.as_string()) # 真正发送邮件就是这里
+    
+    # main方法
+if __name__ == "__main__":
+    whether_to_send, global_ips = get_ip()
+    if whether_to_send:
+        send_an_email(global_ips)
+    else:
+        print("等待,不发送")
---
+++ readme.md
@@ -0,0 +1,91 @@
+### 随身wifi  Debian系统获取ipv6地址并发送邮件通知
+
+
+
+#### 0.安装python相关的内容
+
+```
+root@4G-wifi:~# apt-get install python3 python3-requests python3-gi python3-dbus
+```
+
+
+
+#### 1.复制本仓库的get_ipv6_email.py文件保存到指定目录/usr/local
+
+```
+root@4G-wifi:~# 
+root@4G-wifi:~# cd /usr/local
+root@4G-wifi:/usr/local# mkdir gipv6
+root@4G-wifi:/usr/local# vim get_ipv6_email.py
+
+然后将本项目get_ipv6_email.py文件的内容复制进去
+
+```
+
+
+
+#### 2.然后把你的邮箱信息填写进去
+
+```
+	mail_host = "smtp.163.com" # 这个去邮箱找
+    mail_user = '1xxxxxxx0@163.com' #发送的邮件地址
+    mail_auth_code = 'YxxxxxxxxH' #授权码，不是邮箱的登陆密码
+    mail_sender = mail_user # 用mail_user 作为发送人
+    mail_receivers = ['1xxxxxx70@qq.com'] #收件人list
+```
+
+
+
+#### 3.给get_ipv6_email.py文件赋权
+
+```
+sudo chmod +x get_ipv6_email.py
+
+```
+
+
+
+#### 4.设置开机脚本自启动
+
+```
+sudo vim /etc/rc.local
+
+
+把下边这句填入 exit 0上面
+
+/usr/bin/python3 /usr/local/get_ipv6_email.py > /usr/local/get_ipv6_email.log
+```
+
+
+
+#### 5.最后重启Linux,脚本就能自动运行并打印日志了。
+
+
+
+#### 6.可以看到接收的邮箱里有一个邮件,上边会显示你的ipv6地址
+
+```
+2023-05-19 07:36:41
+该随身wifi的ipv6地址如下:
+["http://[2xx:xxx::11eb]"]
+```
+
+
+
+#### 7.在你的debian上创建网站,然后在浏览器中访问你的ipv6地址:端口号就能访问通了
+
+```
+注意访问地址的格式是:
+ http:[你的ipv6地址]:端口号
+ 
+例如:
+http://[2xx:xxx::11eb]:1526
+```
+
+
+
+参考:
+
+https://zhuanlan.zhihu.com/p/338190964
+
+https://www.linuxprobe.com/linux-python-auto.html
